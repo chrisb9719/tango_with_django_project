b103921f9d465993a84a8596a744853edc68efc4
Christopher Boland <2193014b@student.gla.ac.uk>
2017-01-26 16:36:37 +0000

    Up to and including chapter 5.3

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 print "\n"
 print(__file__)
@@ -68,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -75,6 +77,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [STATIC_DIR,]
 
